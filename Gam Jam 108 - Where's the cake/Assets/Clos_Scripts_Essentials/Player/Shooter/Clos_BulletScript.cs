8d9284bc3236d9620038f3296991e205465e5101
Clossius <Clossius.ShawnRay@gmail.com>
2019-08-02 22:45:02 -0500

    Added Bullet and Shooter Script

---
+++ Gam Jam 108 - Where's the cake/Assets/Clos_Scripts_Essentials/Player/Shooter/Clos_BulletScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clos_BulletScript : MonoBehaviour
+{
+    [Header("Lifespan Settings")]
+    [Tooltip("Should this object be destroyed after given seconds.")]
+    public bool destroyThis = true;
+    [Tooltip("How many seconds until this object is destroyed.")]
+    public float lifeSpan = 3.0f;
+
+    // Movement
+    [Header("Movement Settings")]
+    public bool moveOnSpawn = true;
+    public float speed = 6.0f;
+    public float moveX = 0.0f;
+    public float moveY = 0.0f;
+    public float moveZ = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DestroyAfterSeconds();
+    }
+
+    void FixedUpdate ()
+    {
+        Move();
+    }
+
+    private void DestroyAfterSeconds ()
+    {
+        if (destroyThis) { Destroy(this.gameObject, lifeSpan); }
+    }
+
+    private void Move ()
+    {
+        if (moveOnSpawn) { transform.Translate(new Vector3(moveX, moveY, moveZ) * speed * Time.deltaTime); }
+    }
+}
---
+++ Gam Jam 108 - Where's the cake/Assets/Clos_Scripts_Essentials/Player/Shooter/Clos_ShooterScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This shooter script is to manage how a player
+/// shoots a bullet.
+/// </summary>
+
+public class Clos_ShooterScript : MonoBehaviour
+{
+    [Header("Shooter Settings")]
+    public bool canFire = true;
+    public string fireKey = "space";
+
+    [Header("Objects")]
+    public GameObject bullet;
+    [Tooltip("The transform position that the bullet will spawn in.")]
+    public GameObject bulletSpawn;
+
+
+    private void Update ()
+    {
+        Fire();
+    }
+
+    public void SetCanFire (bool state)
+    {
+        canFire = state;
+    }
+
+    private void SpawnBullet ()
+    {
+        GameObject go = Instantiate(bullet, Vector3.zero, Quaternion.identity);
+        go.transform.position = bulletSpawn.transform.position;
+        go.transform.rotation = bulletSpawn.transform.rotation;
+    }
+
+    private void Fire ()
+    {
+        if (canFire)
+        {
+            if (Input.GetKeyDown(fireKey)) { SpawnBullet(); }
+        }
+    }
+}
