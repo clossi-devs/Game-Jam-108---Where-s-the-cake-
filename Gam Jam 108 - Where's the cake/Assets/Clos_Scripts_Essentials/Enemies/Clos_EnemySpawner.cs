b53844e96980ddb7aaa9a3518084bd5ff16cffad
Clossius <Clossius.ShawnRay@gmail.com>
2019-08-08 22:03:29 -0500

    First Spawner

---
+++ Gam Jam 108 - Where's the cake/Assets/Clos_Scripts_Essentials/Enemies/Clos_EnemySpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clos_EnemySpawner : MonoBehaviour
+{
+    public bool spawnOnStart = false;
+    bool spawning = false;
+    public GameObject enemy;
+    public float secondsToWait = 1.0f;
+    public string targetTag = "Player";
+
+    void Start ()
+    {
+        spawning = spawnOnStart;
+        if (spawning) { SpawnEnemy(); StartCoroutine(WaitToSpawn()); }
+    }
+
+    IEnumerator WaitToSpawn ()
+    {
+        yield return new WaitForSeconds(secondsToWait);
+        if (spawning) { SpawnEnemy(); StartCoroutine(WaitToSpawn()); }
+    }
+
+    private void SpawnEnemy ()
+    {
+        Instantiate(enemy, this.transform.position, Quaternion.identity);
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == targetTag) { spawning = true; SpawnEnemy(); StartCoroutine(WaitToSpawn()); }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if (other.tag == targetTag) { spawning = false; }
+        
+    }
+}
