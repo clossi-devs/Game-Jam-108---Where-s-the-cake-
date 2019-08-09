3036c0a3a8554640a10ded79044fe239df556d69
Jeremy Buentello <jb09056@georgiasouthern.edu>
2019-08-08 23:58:13 -0400

    Adding new animaton interfacing classes

---
+++ Gam Jam 108 - Where's the cake/Assets/Clos_Scripts_Essentials/Animators/BossAnimatorManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAnimatorManager : MonoBehaviour
+{
+    private Animator bossAnimator;
+    void Start()
+    {
+        bossAnimator = GetComponent<Animator>();
+    }
+
+    public void SetIdle()
+    {
+        bossAnimator.SetBool("CanSeePlayer", false);
+    }
+
+    public void SetAttacking()
+    {
+        bossAnimator.SetBool("CanSeePlayer", true);
+    }
+
+    public void SetAttackOnCooldown()
+    {
+        bossAnimator.SetBool("AttackOnCooldown", true);
+    }
+
+    public void SetCooldownComplete()
+    {
+        bossAnimator.SetBool("AttackOnCooldown", false);
+    }
+}
